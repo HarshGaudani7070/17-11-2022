b407515112d1db310cc0006a9271369d5af3e373
harshg2961 <118956982+harshg2961@users.noreply.github.com>
2022-12-05 12:10:36 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main(){
+	
+	int A = 65;
+	int B = 75;
+	
+	if(A>B){
+		printf("B is Minimum");
+	}
+	
+	else{
+		
+		printf("A is Minimum");
+		
+	}
+	
+}
---
+++ 2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+main(){
+	
+	int no;
+	
+	printf("Type No. :- ");
+    
+	scanf("%i",&no);	
+	
+
+	
+	if(0>no){
+		printf("No. is Nagativ");
+	}
+	
+	else if(0<no){ 
+		
+		printf("No. is Positive");
+		
+	}
+	
+	else{
+		
+		printf("No. is Natural");
+		
+		
+	}
+	
+}
+	
